d96425326a27efd29e237c133b905eadf8e3b41b
Herebiondev <contact@herebiondev.com>
2022-05-05 11:38:56 +0200

    Add AppDelegate to the project and hide some unused menu options

--- MultiMap/MultiMap/MultiMapApp.swift
+++ MultiMap/MultiMap/MultiMapApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct MultiMapApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    // Hide the macOS tab system for our app
+                    NSWindow.allowsAutomaticWindowTabbing = false
+                }
         }
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+            CommandGroup(replacing: .undoRedo) {}
+            CommandGroup(replacing: .pasteboard) {}
+        }
+        
     }
 }
