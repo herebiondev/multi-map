62f92ac544cfab1b2d0ddbfec9c2a7b185d969c0
Herebiondev <contact@herebiondev.com>
2022-05-05 14:12:28 +0200

    Add the map

--- MultiMap/MultiMap/ContentView.swift
+++ MultiMap/MultiMap/ContentView.swift
@@ -5,12 +5,24 @@
 //  Created by Herebiondev on 5/5/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 51.507222,
+            longitude: -0.1275
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5
+        )
+    )
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
