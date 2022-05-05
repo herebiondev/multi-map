248d0b4dad163f5d9539adfed14de6b289138031
Herebiondev <contact@herebiondev.com>
2022-05-05 11:36:17 +0200

    Initial commit

---
+++ MultiMap/MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Herebiondev on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
